397393547ea4fced6e45c14b2ab3abee4a524520
Okay <1123362+otse@users.noreply.github.com>
2023-12-21 18:41:10 +0100

    render resize

--- js/renderer.js
+++ js/renderer.js
@@ -1,13 +1,20 @@
+import glob from "./glob.js";
 import { THREE } from "./mall.js";
 var renderer;
 (function (renderer_1) {
     const ad_hoc = 0;
-    var renderer, scene, camera;
+    var renderer, scene, camera, clock;
     function boot() {
     }
     renderer_1.boot = boot;
+    function resize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
     function dom_ready(word) {
         console.log(' dom_ready renderer ');
+        clock = new THREE.Clock();
         scene = new THREE.Scene();
         camera = new THREE.PerspectiveCamera(45, 1024 / 768, 1, 1000);
         camera.position.z = 10;
@@ -19,9 +26,13 @@ var renderer;
         let material = new THREE.MeshLambertMaterial({ wireframe: true, color: 'white' });
         let sphere = new THREE.Mesh(geometry, material);
         scene.add(sphere);
+        window.addEventListener('resize', resize);
     }
     renderer_1.dom_ready = dom_ready;
     function render() {
+        requestAnimationFrame(render);
+        const delta = clock.getDelta();
+        glob.delta = delta;
         renderer.render(scene, camera);
     }
     renderer_1.render = render;
--- src/renderer.ts
+++ src/renderer.ts
@@ -5,15 +5,23 @@ import { THREE } from "./mall.js";
 namespace renderer {
 	const ad_hoc = 0
 
-	var renderer, scene, camera
+	var renderer, scene, camera, clock
 
 	export function boot() {
-		
+
+	}
+
+	function resize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		renderer.setSize(window.innerWidth, window.innerHeight);
 	}
 
 	export function dom_ready(word: string) {
 		console.log(' dom_ready renderer ');
 
+		clock = new THREE.Clock();
 		scene = new THREE.Scene();
 		camera = new THREE.PerspectiveCamera(45, 1024 / 768, 1, 1000);
 		camera.position.z = 10;
@@ -29,9 +37,14 @@ namespace renderer {
 		let sphere = new THREE.Mesh(geometry, material);
 
 		scene.add(sphere);
+
+		window.addEventListener('resize', resize);
 	}
 
 	export function render() {
+		requestAnimationFrame(render);
+		const delta = clock.getDelta();
+		glob.delta = delta;
 		renderer.render(scene, camera);
 	}
 
